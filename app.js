f27e654c7e83b5e4e1e1b2c1a924a6b18dbc28c5
remi <remirobert33530@gmail.com>
2017-04-22 21:24:20 +0100

    add upload image route

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+const uuidV1 = require('uuid/v1');
 
 const app = express();
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-var connections = {};
+var upload = multer({ dest: './files/'});
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var connection;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 io.set('authorization', function (handshakeData, accept) {
     accept(null, true);
@@ -18,18 +24,48 @@ app.post('/analyse', function(req, res) {
     const phone = req.body.phone;
     const username = req.body.username;
     const type = req.body.type;
+/*
+    if (!username || !type) {
+        res.status(401).send("error invalid arguments");
+        return;
+    }
+*/
     console.log("ENTER req body: " + req.body);
+    connection.emit("captureFrame");
+    res.status(200).send("success");
+});
+
+app.post('/capture', function(req, res) {
+    var upload = multer({ dest: 'files/'}).single('file');
+    upload(req, res, function(err) {
+        if (err || !req.body.file) {
+            res.status(401).send("error upload file");
+            return;
+        }
+
+        fs.writeFile("./files/" + uuidV1(), new Buffer(req.body.file, "base64"), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
+        res.status(200).json('yeah');
+    });
 });
 
 app.post('/exit', function(req, res) {
     const phone = req.body.phone;
     const username = req.body.username;
-    console.log("EXIT req body: " + req.body);
+    if (!username) {
+        res.status(401).send("username missing");
+        return;
+    }
+    res.status(200).send("success");
 });
 
 io.on('connection', function(socket) {
+    connection = socket;
     console.log("connection device : " + socket);
-    console.log('a user connected : ' + deviceId);
     io.on('disconnect', function(socket) {
         console.log("disconnect");
     });
